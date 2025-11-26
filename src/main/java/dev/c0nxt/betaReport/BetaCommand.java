357f0b4df27946aa8b46094098bb134f06caa5e4
Papoulidis Konstantinos <pkonstantinoswork@gmail.com>
2025-11-26 21:59:05 +0100

    refactor

---
+++ src/main/java/dev/c0nxt/betaReport/BetaCommand.java
@@ -0,0 +1,26 @@
+package dev.c0nxt.betaReport;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+public record BetaCommand(BetaReport betaReport) implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player) {
+            StringBuilder text = new StringBuilder();
+            Arrays.stream(args).forEach(arg -> {text.append(arg).append(" ");});
+            try {
+                betaReport.getDiscordMessageManager().sendMessage("**" + ((Player) sender).getDisplayName() + ":** " + text);
+            } catch (Exception e) {
+                sender.sendMessage(ChatColor.RED + betaReport.getConfig().getString("error_message"));
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/dev/c0nxt/betaReport/DiscordMessageManager.java
@@ -0,0 +1,38 @@
+package dev.c0nxt.betaReport;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.bukkit.Bukkit;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public record DiscordMessageManager(String webhookUrl) {
+
+    public void sendMessage(String message) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            JsonObject content = new JsonObject();
+            content.addProperty("content", message);
+            JsonObject allowed = new JsonObject();
+            allowed.add("parse", new JsonArray());
+
+            content.add("allowed_mentions", allowed);
+            String body = content.toString();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(webhookUrl))
+                    .POST(HttpRequest.BodyPublishers.ofString(body))
+                    .header("Content-Type", "application/json")
+                    .build();
+
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (InterruptedException | IOException e) {
+            Bukkit.getServer().getLogger().warning(e.getMessage());
+        }
+
+    }
+}
