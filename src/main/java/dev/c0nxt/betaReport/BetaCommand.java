aa7505f23412f63c0ca6f75ed006952418730988
Papoulidis Konstantinos <pkonstantinoswork@gmail.com>
2025-11-26 22:37:18 +0100

    adding updateLink function to command

--- src/main/java/dev/c0nxt/betaReport/BetaCommand.java
+++ src/main/java/dev/c0nxt/betaReport/BetaCommand.java
@@ -12,7 +12,21 @@ import java.util.Arrays;
 public record BetaCommand(BetaReport betaReport) implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
         if (sender instanceof Player) {
+            if (sender.hasPermission("betareport.admin")) {
+                if(args.length == 2){
+                    if (args[0].equalsIgnoreCase("updateLink")) {
+                        betaReport.getConfig().set("discord_webhook", args[1]);
+                        betaReport.saveConfig();
+                    }
+                } else if (args.length == 1) {
+                    sender.sendMessage(ChatColor.RED + "Usage: /beta updateLink <discord_webhook>");
+                } else if(args.length == 0){
+                    sender.sendMessage(ChatColor.RED + "Usage: /beta updateLink <discord_webhook>");
+                }
+                return true;
+            }
             StringBuilder text = new StringBuilder();
             Arrays.stream(args).forEach(x -> text.append(x).append(" "));
             try {
